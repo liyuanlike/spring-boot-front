4914f861289466b792cf64de3eea49b173b87e48
Baby <154518484@qq.com>
2018-07-10 01:12:18 +0800

    optimise.

--- src/main/java/com/github/util/DruidConfig.java
+++ src/main/java/com/github/util/DruidConfig.java
@@ -67,6 +67,16 @@ public class DruidConfig {
 		System.out.println("publicKey:" + arr[1]);
 		System.out.println("password:" + encrypt(arr[0], password));
 		System.out.println("password:" + decrypt(arr[1], encrypt(arr[0], password)));
+
+
+		String DEFAULT_PRIVATE_KEY_STRING = "MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAocbCrurZGbC5GArEHKlAfDSZi7gFBnd4yxOt0rwTqKBFzGyhtQLu5PRKjEiOXVa95aeIIBJ6OhC2f8FjqFUpawIDAQABAkAPejKaBYHrwUqUEEOe8lpnB6lBAsQIUFnQI/vXU4MV+MhIzW0BLVZCiarIQqUXeOhThVWXKFt8GxCykrrUsQ6BAiEA4vMVxEHBovz1di3aozzFvSMdsjTcYRRo82hS5Ru2/OECIQC2fAPoXixVTVY7bNMeuxCP4954ZkXp7fEPDINCjcQDywIgcc8XLkkPcs3Jxk7uYofaXaPbg39wuJpEmzPIxi3k0OECIGubmdpOnin3HuCP/bbjbJLNNoUdGiEmFL5hDI4UdwAdAiEAtcAwbm08bKN7pwwvyqaCBC//VnEWaq39DCzxr+Z2EIk=";
+		String DEFAULT_PUBLIC_KEY_STRING = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBAKHGwq7q2RmwuRgKxBypQHw0mYu4BQZ3eMsTrdK8E6igRcxsobUC7uT0SoxIjl1WveWniCASejoQtn/BY6hVKWsCAwEAAQ==";
+		arr[0] = DEFAULT_PRIVATE_KEY_STRING;
+		arr[1] = DEFAULT_PUBLIC_KEY_STRING;
+		System.out.println("privateKey:" + arr[0]);
+		System.out.println("publicKey:" + arr[1]);
+		System.out.println("password:" + encrypt(arr[0], password));
+		System.out.println("password:" + decrypt(arr[1], encrypt(arr[0], password)));
 	}
 }
 
