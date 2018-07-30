4c60c503db67198c29d9dcfac21e32c9269061fd
姚康明 <154518484@qq.com>
2018-07-30 23:02:05 +0800

    optimise.

--- src/main/java/com/github/util/ThreadPoolConfig.java
+++ src/main/java/com/github/util/ThreadPoolConfig.java
@@ -11,6 +11,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ThreadPoolExecutor;
 
 
@@ -20,10 +22,11 @@ import java.util.concurrent.ThreadPoolExecutor;
 @Configuration
 public class ThreadPoolConfig {
 
-	private static final long MONITOR_RUNNING_PERIOD = 2 * 60 * 1000L;
+	private final Timer timer = new Timer();
+	private static final long MONITOR_RUNNING_PERIOD = 10 * 1000L;
 
 	@Bean
-	public TaskExecutor taskExecutor(TaskScheduler taskScheduler) {
+	public TaskExecutor taskExecutor() {
 		ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
 		taskExecutor.setCorePoolSize(8);
 		taskExecutor.setMaxPoolSize(32);
@@ -31,9 +34,7 @@ public class ThreadPoolConfig {
 		taskExecutor.setThreadNamePrefix("taskExecutor-");
 		taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
 		taskExecutor.initialize();
-
-		taskScheduler.scheduleAtFixedRate(new ThreadPoolMonitor("taskExecutor", taskExecutor.getThreadPoolExecutor()), MONITOR_RUNNING_PERIOD);
-
+		timer.schedule(new ThreadPoolMonitor("taskExecutor", taskExecutor.getThreadPoolExecutor()), MONITOR_RUNNING_PERIOD);
 		return taskExecutor;
 	}
 
@@ -45,16 +46,13 @@ public class ThreadPoolConfig {
 		taskScheduler.setThreadNamePrefix("taskScheduler-");
 		taskScheduler.setWaitForTasksToCompleteOnShutdown(true);
 		taskScheduler.initialize();
-
-		taskScheduler.scheduleAtFixedRate(new ThreadPoolMonitor("taskScheduler", taskScheduler.getScheduledThreadPoolExecutor()), MONITOR_RUNNING_PERIOD);
-
+		timer.schedule(new ThreadPoolMonitor("taskExecutor", taskScheduler.getScheduledThreadPoolExecutor()), MONITOR_RUNNING_PERIOD);
 		return taskScheduler;
 	}
 
-	static class ThreadPoolMonitor implements Runnable {
+	static class ThreadPoolMonitor extends TimerTask {
 
 		private String name;
-		private boolean running = true;
 		private ThreadPoolExecutor executor;
 		private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -62,19 +60,14 @@ public class ThreadPoolConfig {
 			this.name = name;
 			this.executor = executor;
 		}
-		public void shutdown() {
-			this.running = false;
-		}
 		public void run() {
-			while (running) {
-				logger.debug(String
-						.format("[%s monitor] [%d/%d] Active: %d, Completed: %d, Task: %d, isShutdown: %s, isTerminated: %s, Queue.size: %d",
-								this.name,
-								this.executor.getPoolSize(), this.executor.getCorePoolSize(),
-								this.executor.getActiveCount(), this.executor.getCompletedTaskCount(),
-								this.executor.getTaskCount(), this.executor.isShutdown(),
-								this.executor.isTerminated(), this.executor.getQueue().size()));
-			}
+			logger.debug(String
+					.format("[%s monitor] [%d/%d] Active: %d, Completed: %d, Task: %d, isShutdown: %s, isTerminated: %s, Queue.size: %d",
+							this.name,
+							this.executor.getPoolSize(), this.executor.getCorePoolSize(),
+							this.executor.getActiveCount(), this.executor.getCompletedTaskCount(),
+							this.executor.getTaskCount(), this.executor.isShutdown(),
+							this.executor.isTerminated(), this.executor.getQueue().size()));
 		}
 	}
 
