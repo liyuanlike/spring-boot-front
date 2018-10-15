e8dfe4031ffdc5448a11a49fcdad436cbffddfb1
Baby <154518484@qq.com>
2018-10-16 00:49:19 +0800

    optimise.

---
+++ src/main/java/com/github/util/CaffeineCacheConfig.java
@@ -0,0 +1,40 @@
+package com.github.util;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.util.concurrent.TimeUnit;
+
+
+@Configuration
+@EnableCaching
+public class CaffeineCacheConfig {
+
+    @Bean
+    public CacheManager cacheManagerWithCacheLoading(){
+
+        Caffeine caffeine = Caffeine.newBuilder()
+                .recordStats()
+                .initialCapacity(128)
+                .maximumWeight(100)
+                .maximumSize(1024) // maximumSize用来控制cache的最大缓存数量，maximumSize和maximumWeight不可以同时使用
+//                .refreshAfterWrite(5, TimeUnit.SECONDS) // refreshAfterWrite必须设置cacheLoad方法
+                .expireAfterAccess(50, TimeUnit.SECONDS);
+
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setAllowNullValues(true);
+        cacheManager.setCaffeine(caffeine);
+//        cacheManager.setCacheLoader(cacheLoader);
+//        cacheManager.setcach
+//        cacheManager.setCacheNames(getNames());
+        return cacheManager;
+    }
+
+}
